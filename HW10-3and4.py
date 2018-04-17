2f8cfdfeede62283e6da8e388cef666b46301911
Arsalan Ahmed <35544234+arsalan2400@users.noreply.github.com>
2018-04-17 13:53:20 -0400

    Create HW10-3and4.py

---
+++ HW10-3and4.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 17 13:49:10 2018
+
+@author: Aslan
+"""
+
+
+###HOMEWORK QUESTION 3 and 4######
+
+import ParameterClassesAA as P
+import MarkovModelClassesAA as MarkovCls
+import SupportMarkovModel as SupportMarkov
+import SamplePathClasses as PathCls
+import FigureSupport as Figs
+
+SupportMarkov.report_CEA(simOutputs_NONE, simOutputs_ANTICOAG)
+print("Please refer to the table for Question 3.")
+print("To answer Question 4, I'd recommend the anticoagulation drug at a WTP anywhere above $9-10,000.")
+
